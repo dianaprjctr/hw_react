6b5161be27f3dc4d30dbdf852cff3f5740c1d0f3
Diana Dmytriieva <dmitriyevadi@gmail.com>
2025-02-16 21:41:17 +0200

    import React from 'react'; import Card from './Card'; import './App.css';
    function App() {
      const data = [
        {
          img: "https://dzvin.media/wp-content/uploads/2019/11/orig.jpg",
          title: "Перший кіт",
          description: "Це сіамська кішка."
        },
        {
          img: "https://dzvin.media/wp-content/uploads/2019/11/shunshula.jpg",
          title: "Другий кіт",
          description: "Це британська плюшева кішка."
        }
        {
          img: "https://dzvin.media/wp-content/uploads/2019/11/15-1024x680.jpg",
          title: "Третій кіт",
          description: "Це британська плюшева кішка."
        }
        {
          img: "https://dzvin.media/wp-content/uploads/2019/11/695378_1_w_570.jpg",
          title: "Четвертий кіт",
          description: "Це британська довгошерста кішка."
        }
      ];
    
      return (
        <div className="app">
          {data.map((item, index) => (
            <Card
              key={index}
              img={item.img}
              title={item.title}
              description={item.description}
            />
          ))}
        </div>
      );
    }
    
    export default App;

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -14,6 +14,16 @@ function App() {
       title: "Другий кіт",
       description: "Це британська плюшева кішка."
     }
+    {
+      img: "https://dzvin.media/wp-content/uploads/2019/11/15-1024x680.jpg",
+      title: "Третій кіт",
+      description: "Це британська плюшева кішка."
+    }
+    {
+      img: "https://dzvin.media/wp-content/uploads/2019/11/695378_1_w_570.jpg",
+      title: "Четвертий кіт",
+      description: "Це британська довгошерста кішка."
+    }
   ];
 
   return (
