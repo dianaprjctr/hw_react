240235e927e698155dfd2367f92d1ce3c79189c1
Diana Dmytriieva <dmitriyevadi@gmail.com>
2025-02-16 21:30:59 +0200

    import React from 'react'; import Card from './Card'; import './App.css';
    function App() {
      const data = [
        {
          img: "https://images.pexels.com/photos/4587983/pexels-photo-4587983.jpeg",
          title: "Dog in the Park",
          description: "A cute dog enjoying the sun."
        },
        {
          img: "https://images.pexels.com/photos/4588047/pexels-photo-4588047.jpeg",
          title: "Sleepy Dog",
          description: "A sleepy dog taking a nap."
        }
      ];
    
      return (
        <div className="app">
          {data.map((item, index) => (
            <Card
              key={index}
              img={item.img}
              title={item.title}
              description={item.description}
            />
          ))}
        </div>
      );
    }
    
    export default App;

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,32 @@
+import React from 'react';
+import Card from './Card';
+import './App.css';
+
 function App() {
+  const data = [
+    {
+      img: "https://images.pexels.com/photos/4587983/pexels-photo-4587983.jpeg",
+      title: "Dog in the Park",
+      description: "A cute dog enjoying the sun."
+    },
+    {
+      img: "https://images.pexels.com/photos/4588047/pexels-photo-4588047.jpeg",
+      title: "Sleepy Dog",
+      description: "A sleepy dog taking a nap."
+    }
+  ];
+
   return (
-    <h3>Proj React Homework</h3>
+    <div className="app">
+      {data.map((item, index) => (
+        <Card
+          key={index}
+          img={item.img}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
+    </div>
   );
 }
 
